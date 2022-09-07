15511ec2ee092e498ef7aeff9599fd07134b8c6b
AnnyBarba <aebarbap@gmail.com>
2022-09-07 13:29:54 -0500

    actualizacion dashboard

--- sistema_oic/dashboard.php
+++ sistema_oic/dashboard.php
@@ -44,9 +44,9 @@ include('prcd/conn.php');
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="generator" content="">
-    <title>Dashboard | OIC</title>
+    <title>Inicio | Control Interno</title>
 
-    <link rel="icon" type="image/png" href="img/icon.ico"/>
+    <link rel="icon" type="image/png" href="img/inclusion.ico"/>
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/dashboard/">
     <script src="https://kit.fontawesome.com/4d63b5ef28.js" crossorigin="anonymous"></script>
 
@@ -74,46 +74,35 @@ include('prcd/conn.php');
   </head>
   <body>
     <!-- <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"> -->
-    <nav class="navbar navbar-light sticky-top flex-md-nowrap p-0 bg-info text-white">
-        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 text-center text-light" href="#">
-    <!-- <img src="img/TrabajemosJuntosJuventud.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">   -->
-    <h6 class="text-center text-light display-7">OIC</h6>
-    </a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <!-- <input class="form-control form-control-dark w-30" type="text" placeholder="Search" aria-label="Search"> -->
-  
-  
-  <ul class="navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-        <a class="btn btn-outline-light" href="prcd/sort.php" role="button"><i class="fas fa-sign-out-alt"></i> Salir</a>    </li>
-  </ul>
-</nav>
+    <nav class="navbar navbar-light sticky-top flex-md-nowrap p-0 text-white" style="background-color: #917799;">
+        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 text-left text-light" href="#"><img src="img/inclusionlogo.png" width="auto" height="65rem" class="d-inline-block align-top" alt="" ></a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <h4>Sistema para la Evaluación de Control Interno Institucional</h4>
+        <ul class="navbar-nav px-3">
+          <li class="nav-item text-nowrap">
+              <a class="btn btn-outline-light" href="prcd/sort.php" role="button"><i class="fas fa-sign-out-alt"></i> Salir</a>    </li>
+        </ul>
+    </nav>
 
 <div class="container-fluid">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="sidebar-sticky pt-3">
-        
-        <ul class="navbar-nav px-3 text-center">
+        <ul class="navbar-nav px-3 mt-3 text-center">
             <li class="align-middle">
-                   <img src="img/TrabajemosJuntosJuventud.png" width="35%" class="" alt="" loading="lazy">  
-      
+              <!-- <img src="img/Logo Gobierno-Inclusión VColorPNG.png" width="35%" alt=""> -->
             </li>
         </ul>
-
-        <h6 class="sidebar-heading d-flex justify-content-center text-center align-items-center px-3 mt-4 mb-1 text-muted">
-          <span class="">
-          bienvenido<br><i class="fas fa-user"></i> 
+        <p class="sidebar-heading d-flex justify-content-center text-center align-items-center px-3 mt-4 mb-1 text-muted" style="font-size: 1rem;">
+          <span class=""><strong>Bienvenido<br><i class="fas fa-user"></i> 
             <?php
-            
               echo utf8_encode($nombre);
-            
-            ?>
+            ?></strong>
           </span>
-        </h6>
-      <hr>
+        </p>
+        <hr>
         
         <?php
         if($perfil == 1){
@@ -125,18 +114,17 @@ include('prcd/conn.php');
         }
         
         ?>
- 
-           <li class="nav-item">
+
+          <li class="nav-item">
             <a class="nav-link active" href="#">
-              <!-- <span data-feather="home"></span> -->
               <i class="fas fa-laptop-house"></i> 
-              Dashboard <span class="sr-only">(current)</span>
+              Inicio <span class="sr-only">(current)</span>
             </a>
           </li>
           <hr style="color: dimgrey;">
           
           <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>AÑO 2021</span>
+            <span>AÑO 2022</span>
             <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
               <span data-feather="plus-circle"></span>
             </a>
@@ -145,7 +133,6 @@ include('prcd/conn.php');
           <li class="nav-item">
             <a class="nav-link" href="trimestre1.php">
               <span data-feather="layers"></span>
-               
               Primer trimestre
             </a>
           </li>
@@ -256,10 +243,10 @@ include('prcd/conn.php');
    
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-light rounded shadow-sm w-100">
-          <img class="mr-3" src="img/TrabajemosJuntosJuventud.png" alt="" width="48" height="48">
+          <img class="mr-3" src="img/Logo Gobierno-Inclusión VColorPNG.png" alt="" width="auto" height="50">
           <div class="lh-100">
-            <h6 class="mb-0 text-muted lh-100">Instituto de la Juventud del Estado de Zacatecas</h6>
-            <small class="text-muted">Órgano de control interno</small>
+            <h6 class="mb-0 text-muted lh-100">Instituto para la Atención e Inclusión de las Personas con Discapacidad</h6>
+            <small class="text-muted">Control Interno Institucional</small>
           </div>
         </div>
         <div class="btn-toolbar mb-2 mb-md-0">
@@ -290,10 +277,10 @@ include('prcd/conn.php');
   
         <div class="row row-cols-1 row-cols-md-2">
           <div class="col mb-4">
-            <div class="card text-white bg-info mb-6" style="max-width: 36rem; height: 27rem;">
-              <div class="card-header">Enero-Marzo 2020</div>
-              <div class="card-body"><br><br><br>
-                <h1 class="card-title">Primer trimestre</h1>
+            <div class="card text-white mb-6" style="max-width: 36rem; height: 20rem; background-color:#A1A1A1">
+              <div class="card-header"><strong>Enero-Marzo 2022</strong></div>
+              <div class="card-body"><br><br>
+                <h1 class="card-title mb-4">Primer trimestre</h1>
                 <?php
                   $observaciones1 ="SELECT * FROM actividad WHERE responsable = '$id'";
                   $resultado_observaciones1 = $conn->query($observaciones1);
@@ -301,31 +288,31 @@ include('prcd/conn.php');
                         echo '<p class="card-text"><i class="bi bi-info-circle-fill"></i> '.$row_observaciones1['observaciones'].'</p>';
                       }
                 ?>
-                <!-- <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores aliquid, dolores distinctio similique excepturi omnis qui accusantium sit molestiae ad dignissimos voluptatem vitae! Ipsam molestiae culpa quia quas libero necessitatibus..</p> -->
-                <p><a href="trimestre1.php" type="button" class="btn btn-warning"><i class="fas fa-edit"></i> Editar...</a></p>
+                <p><a href="trimestre1.php" type="button text-right" class="btn btn-dark mt-3"><i class="fas fa-edit"></i> Editar...</a></p>
               </div>
             </div>
           </div>
           <div class="col mb-4">
-            <div class="card text-white bg-info mb-6" style="max-width: 36rem;height: 27rem;">
-              <div class="card-header">Abril-Junio 2020</div>
-              <div class="card-body"><br><br><br>
-                <h1 class="card-title">Segundo trimestre</h1>
+            <div class="card text-white mb-6" style="max-width: 36rem; height: 20rem; background-color:#A1A1A1">
+              <div class="card-header"><strong>Abril-Junio 2022</strong></div>
+              <div class="card-body"><br><br>
+                <h1 class="card-title mb-4">Segundo trimestre</h1>
                 <?php
                   $observaciones2 ="SELECT * FROM actividad WHERE responsable = '$id'";
                   $resultado_observaciones2 = $conn->query($observaciones2);
                       while($row_observaciones2 = $resultado_observaciones2->fetch_assoc()){
                         echo '<p class="card-text"><i class="bi bi-info-circle-fill"></i> '.$row_observaciones2['observaciones2'].'</p>';
                       }
-                ?>                <p><a href="trimestre2.php" type="button" class="btn btn-warning"><i class="fas fa-edit"></i> Editar...</a></p>
+                ?>                
+                <p><a href="trimestre2.php" type="button" class="btn btn-dark mt-3"><i class="fas fa-edit"></i> Editar...</a></p>
               </div>
             </div>
           </div>
           <div class="col mb-4">
-            <div class="card text-white bg-info mb-6" style="max-width: 36rem;height: 27rem;">
-              <div class="card-header">Julio-Septiembre 2020</div>
-              <div class="card-body"><br><br><br>
-                <h1 class="card-title">Tercer trimestre</h1>
+            <div class="card text-white mb-6" style="max-width: 36rem; height: 20rem; background-color:#A1A1A1">
+              <div class="card-header"><strong>Julio-Septiembre 2022</strong></div>
+              <div class="card-body"><br><br>
+                <h1 class="card-title mb-4">Tercer trimestre</h1>
                 <?php
                   $observaciones3 ="SELECT * FROM actividad WHERE responsable = '$id'";
                   $resultado_observaciones3 = $conn->query($observaciones3);
@@ -333,22 +320,23 @@ include('prcd/conn.php');
                         echo '<p class="card-text"><i class="bi bi-info-circle-fill"></i> '.$row_observaciones3['observaciones3'].'</p>';
                       }
                 ?>                
-                <p><a href="trimestre3.php" type="button" class="btn btn-warning"><i class="fas fa-edit"></i> Editar...</a></p>
+                <p><a href="trimestre3.php" type="button" class="btn btn-dark mt-3"><i class="fas fa-edit"></i> Editar...</a></p>
               </div>
             </div>
           </div>
           <div class="col mb-4">
-            <div class="card text-white bg-info mb-6" style="max-width: 36rem;height: 27rem;">
-              <div class="card-header">Octubre-Diciembre 2020</div>
-              <div class="card-body"><br><br><br>
-                <h1 class="card-title">Cuarto trimestre</h1>
+            <div class="card text-white mb-6" style="max-width: 36rem; height: 20rem; background-color:#A1A1A1">
+              <div class="card-header"><strong>Octubre-Diciembre 2022</strong></div>
+              <div class="card-body"><br><br>
+                <h1 class="card-title mb-4">Cuarto trimestre</h1>
                 <?php
                   $observaciones4 ="SELECT * FROM actividad WHERE responsable = '$id'";
                   $resultado_observaciones4 = $conn->query($observaciones4);
                       while($row_observaciones4 = $resultado_observaciones4->fetch_assoc()){
-                        echo '<p class="card-text"><i class="bi bi-info-circle-fill"></i> '.$row_observaciones3['observaciones3'].'</p>';
+                        echo '<p class="card-text"><i class="bi bi-info-circle-fill"></i> '.$row_observaciones4['observaciones4'].'</p>';
                       }
-                ?>                 <p><a href="trimestre4.php" type="button" class="btn btn-warning"><i class="fas fa-edit"></i> Editar...</a></p>
+                ?>                 
+                <p><a href="trimestre4.php" type="button" class="btn btn-dark mt-3"><i class="fas fa-edit"></i> Editar...</a></p>
               </div>
             </div>
           </div>
