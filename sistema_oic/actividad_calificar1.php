e8a1902b1ae55ae75255c10e64f49975ac955a89
AnnyBarba <aebarbap@gmail.com>
2022-09-09 11:25:50 -0500

    actualizacion front-end actividad_calificar

--- sistema_oic/actividad_calificar1.php
+++ sistema_oic/actividad_calificar1.php
@@ -1,4 +1,5 @@
 <?php
+include('prcd/conn.php');
 session_start();
 
 if (isset($_SESSION['usr'])) {
@@ -24,15 +25,13 @@ if (isset($_SESSION['usr'])) {
     header('Location: prcd/sort.php');
     die();
 }
-
-include('prcd/conn.php');
-
 // variables de sesión
 
-    $usuario = $_SESSION['usr'];
-    $id = $_SESSION['id'];
-    $perfil = $_SESSION['perfil'];
-    $nombre = $_SESSION['nombre'];
+$usuario = $_SESSION['usr'];
+$id = $_SESSION['id'];
+$perfil = $_SESSION['perfil'];
+$nombre = $_SESSION['nombre'];
+
 
 ?>
 
@@ -44,10 +43,14 @@ include('prcd/conn.php');
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="generator" content="">
-    <title>Inicio | Control Interno</title>
+    <title>Dashboard | OIC</title>
 
-    <link rel="icon" type="image/png" href="img/inclusion.ico"/>
+    <link rel="icon" type="image/png" href="img/icon.ico"/>
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/dashboard/">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    
     <script src="https://kit.fontawesome.com/4d63b5ef28.js" crossorigin="anonymous"></script>
 
     <!-- Bootstrap core CSS -->
@@ -74,254 +77,127 @@ include('prcd/conn.php');
   </head>
   <body>
     <!-- <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"> -->
-    <nav class="navbar navbar-light sticky-top flex-md-nowrap p-0 text-white" style="background-color: #917799;">
-        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 text-left text-light" href="#"><img src="img/inclusionlogo.png" width="auto" height="65rem" class="d-inline-block align-top" alt="" ></a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <h4>Sistema para la Evaluación de Control Interno Institucional</h4>
-        <ul class="navbar-nav px-3">
-          <li class="nav-item text-nowrap">
-              <a class="btn btn-outline-light" href="prcd/sort.php" role="button"><i class="fas fa-sign-out-alt"></i> Salir</a>    </li>
-        </ul>
-    </nav>
+    <nav class="navbar navbar-light sticky-top flex-md-nowrap p-0 bg-info text-white">
+        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 text-center text-light" href="#">
+    <img src="img/Logo Gobierno-Inclusión VColorPNG.png" width="auto" height="30" class=" align-left" alt="" loading="lazy">
+    <h6 class="text-center text-light display-7">OIC</h6>
+    </a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!-- <input class="form-control form-control-dark w-30" type="text" placeholder="Search" aria-label="Search"> -->
+  
+  
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+        <a class="btn btn-outline-light btn-sm" href="prcd/sort.php" role="button"><i class="fas fa-sign-out-alt"></i> Salir</a>    </li>
+  </ul>
+</nav>
 
 <div class="container-fluid">
   <div class="row">
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="sidebar-sticky pt-3">
-        <ul class="navbar-nav px-3 mt-3 text-center">
+        
+        <ul class="navbar-nav px-3 text-center">
             <li class="align-middle">
-              <!-- <img src="img/Logo Gobierno-Inclusión VColorPNG.png" width="35%" alt=""> -->
+                   <img src="img/TrabajemosJuntosJuventud.png" width="35%" class="" alt="" loading="lazy">  
+      
             </li>
         </ul>
-        <p class="sidebar-heading d-flex justify-content-center text-center align-items-center px-3 mt-4 mb-1 text-muted" style="font-size: 1rem;">
-          <span class=""><strong>Bienvenido<br><i class="fas fa-user"></i> 
+<hr>
+
+        <h6 class="sidebar-heading d-flex justify-content-center text-center align-items-center px-3 mt-4 mb-1 text-muted">
+          <span class="">
+          bienvenido<br><i class="fas fa-user"></i> 
             <?php
-              echo ($nombre);
-            ?></strong>
+            
+              echo utf8_encode($nombre);
+            
+            ?>
           </span>
-        </p>
-        <hr>
-        
-        <?php
-/*         if($perfil == 1){
-          echo '<ul class="nav flex-column">';
-        }
-
-        else{
-          echo '<ul class="nav flex-column" hidden>';
-        }
-         */
-        ?>
-
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              <i class="fas fa-laptop-house"></i> 
-              Inicio <span class="sr-only">(current)</span>
-            </a>
-          </li>
-
-
-          <hr style="color: dimgrey;">
-        
-        <?php
-/*         if($perfil == 1){
-          echo '<ul class="nav flex-column">';
-        }
-
-        else{
-          echo '<ul class="nav flex-column" hidden>';
-        }
-         */
-        ?>
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Años</span>
-            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-              <span data-feather="plus-circle"></span>
-            </a>
-          </h6>
-
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre1.php">
-              <span data-feather="layers"></span>
-              2022
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre2.php">
-              <span data-feather="layers"></span>
-              2023
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre3.php">
-              <span data-feather="layers"></span>
-              2024
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre4.php">
-              <span data-feather="layers"></span>
-              2025
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre4.php">
-              <span data-feather="layers"></span>
-              2026
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre4.php">
-              <span data-feather="layers"></span>
-              2027
-            </a>
-          </li>
-        </ul>
-
-        <?php
-/*         if($perfil == 1){
-          echo '<ul class="nav flex-column">';
-        }
-
-        else{
-          echo '<ul class="nav flex-column" hidden>';
-        }
-         */
-        ?>
-
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>AÑO 2022</span>
-            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-              <span data-feather="plus-circle"></span>
-            </a>
-          </h6>
-
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre1.php">
-              <span data-feather="layers"></span>
-              Primer trimestre
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre2.php">
-              <span data-feather="layers"></span>
-              Segundo trimestre
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre3.php">
-              <span data-feather="layers"></span>
-              Tercer trimestre
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="trimestre4.php">
-              <span data-feather="layers"></span>
-              Cuarto trimestre
-            </a>
-          </li>
-        </ul>
-
-        <?php
-/*         if($perfil == 2){
-          echo '<ul class="nav flex-column">';
-        }
-
-        else{
-          echo '<ul class="nav flex-column" hidden>';
-        } */
-        
-        ?>
-        <!-- <ul class="nav flex-column mb-2"> -->
-        
-        
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Departamentos responsables</span>
-            <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-              <span data-feather="plus-circle"></span>
-            </a>
-          </h6>
-          <?php 
-          $tabla="SELECT * FROM area";
-                      $resultadotabla = $conn->query($tabla);
-                      while($row = $resultadotabla->fetch_assoc()){
-                          
-                        echo '<li class="nav-item">';
-                        echo ('<a class="nav-link" href="area.php?area='.$row['resp'].'">
-                          <span data-feather="file-text"></span>
-                            '.$row['area'].'
-                            
-                            </a>');
-                        echo '</li>';                      
-                      }
-
-                      ?>
-          </ul>
-        
-
-       
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          
-        <?php
-/*         if($perfil == 2){
-          echo '<span>Plantillas</span>';
-        }
-
-        else{
-          echo '<span></span>';
-        } */
-        
-        ?>
-          <!-- <span>Plantillas</span> -->
-          
-          <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
         </h6>
 
-        <?php
-/*         if($perfil == 2){
-          echo '<ul class="nav flex-column">';
-        }
+<ul class="nav flex-column">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">
+       <!-- <span data-feather="home"></span> -->
+       <i class="fas fa-laptop-house"></i> 
+       Dashboard <span class="sr-only">(current)</span>
+     </a>
+   </li>
+   <hr style="color: dimgrey;">
+   
+   <!-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+     <span>AÑO 2021</span>
+     <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+       <span data-feather="plus-circle"></span>
+     </a>
+   </h6> -->
 
-        else{
-          echo '<ul class="nav flex-column" hidden>';
-        } */
+   <!-- <li class="nav-item">
+     <a class="nav-link" href="trimestre1.php">
+       <span data-feather="layers"></span>
         
-        ?>
-        <!-- <ul class="nav flex-column mb-2"> -->
-          <li class="nav-item">
-            <a class="nav-link" href="modificar.php">
-              <span data-feather="file-text"></span>
-              Modificar
-            </a>
-          </li>
-          
-        </ul>
-       
+       Primer trimestre
+     </a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="trimestre2.php">
+       <span data-feather="layers"></span>
+       Segundo trimestre
+     </a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="trimestre3.php">
+       <span data-feather="layers"></span>
+       Tercer trimestre
+     </a>
+   </li>
+   <li class="nav-item">
+     <a class="nav-link" href="trimestre4.php">
+       <span data-feather="layers"></span>
+       Cuarto trimestre
+     </a>
+   </li> -->
+ </ul>
 
+ <!-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+   <span>Plantillas</span>
+   <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+     <span data-feather="plus-circle"></span>
+   </a>
+ </h6>
+ <ul class="nav flex-column mb-2">
+   <li class="nav-item">
+     <a class="nav-link" href="modificar.php">
+       <span data-feather="file-text"></span>
+       Modificar
+     </a>
+   </li> -->
+   
+ </ul>    
       </div>
-      
     </nav>
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-      
-   
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-      <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-light rounded shadow-sm w-100">
-          <img class="mr-3" src="img/Logo Gobierno-Inclusión VColorPNG.png" alt="" width="auto" height="50">
+        <!-- <h1 class="h3">MODIFICAR ACTIVIDAD</h1> -->
+        
+
+        <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-light rounded shadow-sm w-100">
+          <img class="mr-3" src="img/Logo Gobierno-Inclusión VColorPNG.png" alt="" width="auto" height="48">
           <div class="lh-100">
-            <h6 class="mb-0 text-muted lh-100">Instituto para la Atención e Inclusión de las Personas con Discapacidad</h6>
-            <small class="text-muted">Control Interno Institucional</small>
+            <h6 class="mb-0 text-muted lh-100">INCLUSIÓN</h6>
+            <small class="text-muted">Órgano de control interno</small>
           </div>
         </div>
+        
         <div class="btn-toolbar mb-2 mb-md-0">
-          <!-- <div class="btn-group mr-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Reporte PDF</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Reporte EXCEL</button>
-          </div> -->
+          <div class="btn-group mr-2">
+            <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Reporte PDF</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Reporte EXCEL</button> -->
+          </div>
          
         </div>
       </div>
@@ -675,6 +551,7 @@ include('prcd/conn.php');
           </table>
 
       </div> <!-- div table-responsive -->
+      </div>
 
 
     </main>
